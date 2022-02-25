8885c783f573a190f4ca76092d77e178f2422309
Johnny Phan <jphan345@gmail.com>
2022-02-25 11:20:40 -0800

    navbar :)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
       <Footer />
     </div>
   );
