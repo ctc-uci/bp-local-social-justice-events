fa6614b77cad544e5bd6b363181b2a42e8e37848
Johnny Phan <jphan345@gmail.com>
2022-04-14 16:24:03 -0700

    resources and about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './About.css';
 
 function About() {
   return (
-    <div className="About">
-      <h1>About Page</h1>
+    <div className="about">
+      <h1 className="title">About us!</h1>
+      <p className="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Sodales ut eu sem integer vitae justo eget. Risus nullam eget
+        felis eget. Blandit libero volutpat sed cras ornare arcu dui.
+      </p>
     </div>
   );
 }
--- client/src/pages/Resources.jsx
+++ client/src/pages/Resources.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Resources.css';
 
 function Resources() {
   return (
     <div className="Resources">
       <h1>Resources Page</h1>
+      <h3>Social Cause </h3>
+      <p> text text text </p>
     </div>
   );
 }
